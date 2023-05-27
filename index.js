1fb1569048ad3f95f70207c6b7098607e718f457
Albikras <124819029+Albikras@users.noreply.github.com>
2023-05-27 15:58:52 -0400

    added mockup code for the creating of the logo

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const fs = require("fs");
 // const shapes = require("./lib/shapes");
 const questions = require("./questions");
 const inquirer = require("inquirer");
-const maxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+const maxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
 
 inquirer.registerPrompt("max", maxLengthInputPrompt);
 
 function writeFile(file) {
   inquirer.prompt(questions).then((data) => {
     console.log(data);
+
+    fs.writeFile("./examples/logo.svg", file, (err) => {
+      err ? console.log("error") : console.log("Generated logo.svg");
+    });
   });
 }
 

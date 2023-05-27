151fb27baea6cb572b623448f2c3d12bab6b0c8f
Albikras <124819029+Albikras@users.noreply.github.com>
2023-05-27 15:52:59 -0400

    added a new package for a max-length input and fixed the question

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const fs = require("fs");
-const shapes = require("./lib/shapes");
-const question = require("./questions");
+// const shapes = require("./lib/shapes");
+const questions = require("./questions");
 const inquirer = require("inquirer");
+const maxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+
+inquirer.registerPrompt("max", maxLengthInputPrompt);
 
 function writeFile(file) {
   inquirer.prompt(questions).then((data) => {
     console.log(data);
   });
 }
+
+writeFile();
--- questions.js
+++ questions.js
@@ -1,36 +1,28 @@
-
-const questions = [{
-
-    type: 'maxlength-input',
-    name: 'logotitle',
-    message: 'Enter a logo title cannot be more then 3 characters',
-    maxLength: 3
-},
-{
-    type: 'input',
-    name: 'textColor',
-    message: 'Enter a color for your text(can be the color or hexadecimal value)'
-},
-{
-    type: 'list',
-    name: 'shapes',
-    message: 'Pick which shape your want for your logo',
-    choices: ['circle','triangle','square']
-},
-{
-    type: 'input',
-    name: 'shapeColor',
-    message: 'Enter a color for your shape(can be the color or hexadecimal value)'
-},
+const questions = [
+  {
+    type: "max",
+    name: "logotitle",
+    message: "Enter a logo title cannot be more then 3 characters",
+    maxLength: 3,
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message:
+      "Enter a color for your text(can be the color or hexadecimal value)",
+  },
+  {
+    type: "list",
+    name: "shapes",
+    message: "Pick which shape your want for your logo",
+    choices: ["circle", "triangle", "square"],
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message:
+      "Enter a color for your shape(can be the color or hexadecimal value)",
+  },
 ];
 
-
-
-
-
-
-
-
-
-
-module.exports = questions
+module.exports = questions;

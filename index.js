df0b7b762f60461d6d5341ef7ecbd31e2c780588
Albikras <124819029+Albikras@users.noreply.github.com>
2023-05-29 16:14:53 -0400

    addeed 1 new js file for a seperate class and then also made it so all the classes now have full functionallity, and wrote the code to actually create my svg logo

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const fs = require("fs");
-// const shapes = require("./lib/shapes");
 const questions = require("./questions");
 const inquirer = require("inquirer");
 const maxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
-const logoGenerator = require("./lib/shapes");
+const { Triangle, Square, Circle } = require("./lib/shapes");
+const { SVG } = require("./lib/newSVG");
 
 inquirer.registerPrompt("max", maxLengthInputPrompt);
 
 function writeFile() {
   inquirer.prompt(questions).then((data) => {
     console.log(data);
+    const color = data.shapeColor;
+    let svgShape;
+    const textColor = data.textColor;
+    const text = data.logotitle;
+    switch (data.shapes) {
+      case "circle":
+        svgShape = new Circle(color);
+        break;
+      case "triangle":
+        svgShape = new Triangle(color);
+        break;
+      case "square":
+        svgShape = new Square(color);
+        break;
+    }
+    let x = svgShape.render();
 
-    const logoContent = logoGenerator(data);
+    const svg = new SVG(textColor, text, x);
 
-    fs.writeFile("./examples/logo.svg", logoContent, (err) => {
+    let y = svg.render();
+
+    fs.writeFile("./examples/logo.svg", y, (err) => {
       err ? console.log("error") : console.log("Generated logo.svg");
     });
   });
